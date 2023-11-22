9356dabbae4afb8c42cafeccc9d705de6c4401bc
agakshay304 <agakshay304@gmail.com>
2023-11-22 18:42:43 +0530

    Initial Commit

---
+++ ExpenseShare/Helpers/Extensions.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension Double {
+    
+    var formattedCurrencyText: String {
+        return Utils.numberFormatter.string(from: NSNumber(value: self)) ?? "0"
+    }
+    
+}
---
+++ ExpenseShare/Helpers/Utils.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct Utils {
+    
+    static let dateFormatter: RelativeDateTimeFormatter = {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .full
+        return formatter
+    }()
+    
+    static let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.isLenient = true
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+    
+}
