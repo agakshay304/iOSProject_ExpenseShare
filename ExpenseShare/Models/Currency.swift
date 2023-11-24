6ed85b2b706830102962bc93870acaad383d2f03
agakshay304 <agakshay304@gmail.com>
2023-11-25 02:13:16 +0530

    Bugs Fixed

---
+++ ExpenseShare/Models/Currency.swift
@@ -0,0 +1,9 @@
+//
+//  Currency.swift
+//  ExpenseShare
+//
+//  Created by Akshay Gupta on 25/11/23.
+//  Copyright © 2023 Alfian Losari. All rights reserved.
+//
+
+import Foundation
---
+++ ExpenseShare/Models/Friend.swift
@@ -0,0 +1,9 @@
+//
+//  Friend.swift
+//  ExpenseShare
+//
+//  Created by Akshay Gupta on 25/11/23.
+//  Copyright © 2023 Alfian Losari. All rights reserved.
+//
+
+import Foundation
